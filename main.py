78673da2843982a127e3a0209e3510b7eb38c350
Heraldo <hjacauna@gmail.com>
2026-02-18 10:22:56 -0300

    feat: acessar endpoint das fases de oportunidades

--- main.py
+++ main.py
@@ -7,6 +7,53 @@ import json
 
 st.set_page_config(page_title="CRM - Oportunidades", layout="wide")
 
+def get_fases():
+    # Suas credenciais da Omie
+    APP_KEY = st.secrets["app_key"]
+    APP_SECRET = st.secrets["app_secret"]
+
+    # Endpoint da API
+    URL = "https://app.omie.com.br/api/v1/crm/fases/"
+
+    # Estrutura do corpo da requisição conforme o cURL fornecido
+    payload = {
+        "call": "ListarFases",
+        "param": [
+            {
+                "pagina": 1,
+                "registros_por_pagina": 10,
+            }
+        ],
+        "app_key": APP_KEY,
+        "app_secret": APP_SECRET
+    }
+
+    # Cabeçalhos obrigatórios
+    headers = {
+        "Content-type": "application/json"
+    }
+
+    try:
+        # Envia a requisição POST para a API
+        response = requests.post(URL, headers=headers, data=json.dumps(payload))
+        
+        # Verifica se houve erro (4xx ou 5xx)
+        response.raise_for_status()
+        
+        # Converte a resposta para JSON
+        data = response.json()
+
+        df_fases = pd.DataFrame(data["cadastros"])
+
+        return df_fases # Exibe as primeiras linhas do DataFrame
+
+    except requests.exceptions.HTTPError as err:
+        print(f"Erro na API: {err}")
+        print(f"Detalhes do erro: {response.text}")
+    except Exception as e:
+        print(f"Ocorreu um erro inesperado: {e}")
+
+
 def get_opportunities():
 
     # Suas credenciais da Omie
@@ -60,7 +107,7 @@ def get_opportunities():
 
         df_filtro_oportunidade = df_filtro[["nCodVendedor", "nCodConta", "cDesOp", "nCodStatus", "nAnoPrev", "nMesPrev", "ticket Meses", "ticket Produtos", "ticket Recorrencia", "ticket Servico"]]
 
-        return df_filtro_oportunidade.head()  # Exibe as primeiras linhas do DataFrame
+        return df_filtro_oportunidade # Exibe as primeiras linhas do DataFrame
 
     except requests.exceptions.HTTPError as err:
         print(f"Erro na API: {err}")
@@ -72,3 +119,4 @@ def get_opportunities():
 
 get_opportunities()
 
+##get_fases()
